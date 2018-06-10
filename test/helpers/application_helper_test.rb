259be30ea87e50a52baa3fb4b7576f55319bc117
Koki Nakamichi <kokinakamichi0617@g.ecc.u-tokyo.ac.jp>
2018-06-10 18:47:43 +0900

    modify helper/layout test + gem files

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title("Home")
-    assert_equal full_title("Help")
-    assert_equal full_title("About")
-    assert_equal full_title("Contact")
-    assert_equal full_title("Sign up")
+    # assert_equal, <expected result>, <result written in original code> (see below for example)
+    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("About"), "About | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Contact"), "Contact | Ruby on Rails Tutorial Sample App"
+    assert_equal full_title("Sign up"), "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
+
+# example code
+# result = 1+1
+# assert_equal 2, result
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,6 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+
     get help_path
     assert_select "title", full_title("Help")
     get about_path
