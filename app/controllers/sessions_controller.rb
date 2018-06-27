5d4c7b1a1d1ff806a185d49ce4c1b9a2d2f8779c
Koki Nakamichi <kokinakamichi0617@g.ecc.u-tokyo.ac.jp>
2018-06-27 14:54:05 +0900

    fix integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      login user
+      log_in user
       redirect_to user
       # Log the user in and redirect to the user's show page.
     else
